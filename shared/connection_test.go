30eff3cf80c4f6573670d586d7dba439f1ac06db
MikaNikulin <mika@nikulin.fi>
2018-12-10 14:56:50 +0200

    Disabling MongoDB URL testing during build process #8

--- shared/connection_test.go
+++ shared/connection_test.go
@@ -18,7 +18,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+//	"github.com/stretchr/testify/require"
 )
 
 func TestRedactMongoUri(t *testing.T) {
